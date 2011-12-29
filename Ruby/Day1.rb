117c79e57d1d91c79bbdac8ea660288f421de52f
Eric Smalling <eric@viarun.com>
2011-12-28 23:59:47 -0600

    Day 1 exercise solutions

---
+++ Ruby/Day1.rb
@@ -0,0 +1,12 @@
+puts "Hello, world"
+
+index = "Hello, Ruby".index("Ruby")
+puts "index = #{index}"
+
+10.times {puts 'Eric'}
+
+x = 0
+while x < 10
+  x = x + 1
+  puts "This is sentence number #{x}"
+end
