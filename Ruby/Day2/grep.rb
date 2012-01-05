b5c63e798ad066e3271af06448fd5680fdfbe868
Eric Smalling <eric@viarun.com>
2012-01-05 05:53:18 -0600

    Adding multifile capability to grep

--- Ruby/Day2/grep.rb
+++ Ruby/Day2/grep.rb
@@ -1,11 +1,14 @@
 #!/usr/bin/ruby
 regex = Regexp.new(ARGV[0])
-filename = ARGV[1]
-lineno = 0
+ARGV.shift();
 
-File.open(filename) do |file|
-  file.each_line do |line|
-    lineno += 1
-    puts "#{lineno}: #{line}" if line =~ regex
+ARGV.each{|filename|
+  lineno = 0
+
+  File.open(filename) do |file|
+    file.each_line do |line|
+      lineno += 1
+      puts "#{filename} #{lineno}: #{line}" if line =~ regex
+    end
   end
-end
+}
