64a94bfa655b00ecab9cb74041309a855ec025de
Eric Smalling <eric@viarun.com>
2012-02-08 22:53:14 -0600

    Prolog Day 1

---
+++ Prolog/books.pl
@@ -0,0 +1,4 @@
+author(genesis, moses).
+author(exodus, moses).
+author(acts, paul).
+author(job, unknown).
---
+++ Prolog/musicians.pl
@@ -0,0 +1,19 @@
+musician_plays('geddy lee', bass).
+musician_plays('geddy lee', keyboards).
+musician_plays('alex lifeson', guitar).
+musician_plays('neil peart', percussion).
+musician_plays('sting', bass).
+musician_plays('stewart copeland', percussion).
+musician_plays('andy summers', guitar).
+musician_plays('eric smalling', percussion).
+
+musician_genre('geddy lee', 'prog rock').
+musician_genre('alex lifeson', 'prog rock').
+musician_genre('neil peart', 'prog rock').
+musician_genre('sting', 'rock').
+musician_genre('sting', 'easy listening').
+musician_genre('andy summers', 'rock').
+musician_genre('stewart copeland', 'rock').
+musician_genre('stewart copeland', 'soundtracks').
+musician_genre('stewart copeland', 'orchestra').
+musician_genre('eric smalling', 'marching band').
